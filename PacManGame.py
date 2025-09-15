a836209010b6e89643baf7ecfbad5e92a03b6a49
Ben Arnold <s.ben.arnold@kmsu.ch>
2025-09-15 15:46:04 +0200

    points implementiert

--- PacManGame.py
+++ PacManGame.py
@@ -1,6 +1,7 @@
 # Example file showing a circle moving on screen
 import pygame
-from PacMan import PacMan
+from pacman import PacMan
+from points import Point, punkte
 
 # pygame setup
 pygame.init()
@@ -20,17 +21,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 player1.change_direction('UP')
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 player1.change_direction('DOWN')
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 player1.change_direction('LEFT')
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 player1.change_direction('RIGHT')
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
+
+    for p in punkte.values():
+        pygame.draw.circle(screen, "yellow", (p.x * 31 + 15, p.y * 31 + 15), 2)
+        
 
     elapsed += dt
 
@@ -49,7 +54,10 @@ while running:
     if elapsed > 0.5:
         elapsed -= 0.5
         player1.move()
+        if (player1.x, player1.y) in punkte:
+            del punkte[(player1.x,player1.y)]
         player_pos = pygame.Vector2(player1.x * 31 + 15, player1.y * 31 + 15)
+
     player_pos += pygame.Vector2(x * (elapsed / 0.5) * 31, y * (elapsed / 0.5) * 31)
 
 
